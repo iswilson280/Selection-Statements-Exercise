db996e83d2df68027501fbdd7162225f9c6da7d0
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-06-21 14:08:44 -0500

    Selection Statement Excercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,34 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var r = new Random();
+            var favNumber = r.Next(1, 3);
+
+
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine ("too high");
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("you guessed correctly");
+            }
+        }   
+
     }
 }
